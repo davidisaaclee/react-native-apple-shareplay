820b077774e10f47314f96a3c6b975aa8ed198cd
David Lee <david.isaac.lee@gmail.com>
2025-08-05 17:04:36 -0700

    Remove file leftover from tech proof

--- ios/SwiftFile.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-@objc public class SwiftFile: NSObject {
-    @objc public func multiplyByThree(_ x: NSNumber) -> NSNumber {
-        return NSNumber(value: x.intValue * 3)
-    }
-}
